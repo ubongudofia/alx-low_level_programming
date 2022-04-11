4e277c84e32522252f338947d17a778592deedd3
ubongudofia <udofiaubong10@gmail.com>
2022-04-11 16:22:54 -0700

    Print comb

---
+++ 0x01-variables_if_else_while/0-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - program that prints all possible combinations
+  *
+  * Return: Always return 0
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i % 10 + '0');
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
